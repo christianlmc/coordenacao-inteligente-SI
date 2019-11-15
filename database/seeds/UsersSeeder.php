fc39b53b8f9b209b4ef4cbaf7df2528b5ea8dfcc
Christian Luis <christianlmc1@gmail.com>
2019-11-15 20:13:15 -0300

    Adicionando seeder para usuario admin

---
+++ database/seeds/UsersSeeder.php
@@ -0,0 +1,24 @@
+<?php
+
+use App\User;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Hash;
+
+class UsersSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        $admin = [
+            'name' => 'Admin',
+            'email' => 'admin@unb.br',
+            'password' => Hash::make('123123')
+        ];
+
+        User::create($admin);
+    }
+}
