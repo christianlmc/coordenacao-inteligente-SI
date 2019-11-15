e40df5a4c4fb3da49c319eff3435d1f92a8e67e0
Christian Luis <christianlmc1@gmail.com>
2019-11-15 20:55:38 -0300

    Criando rotas do menu principal

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -5,7 +5,7 @@
     <div class="row justify-content-center">
         <div class="col-md-8">
             <div class="card">
-                <div class="card-header">Dashboard</div>
+                <div class="card-header">Menu Principal</div>
 
                 <div class="card-body">
                     @if (session('status'))
@@ -14,7 +14,16 @@
                         </div>
                     @endif
 
-                    You are logged in!
+                    <h3>Fila de Espera</h3>
+                    <hr/>
+                    <a href="{{ route('filas.agendar') }}" role="button" class="btn text-uppercase btn-primary btn-lg btn-block">Agendar Horário com Coordenador</a>
+                    <a href="{{ route('filas.index') }}" role="button" class="btn text-uppercase btn-primary btn-lg btn-block">Visualizar Fila do Coordenador</a>
+
+                    <h3 class="mt-5">Turmas e Trocas</h3>
+                    <hr/>
+                    <a href="{{ route('disciplinas.buscar') }}" role="button" class="btn text-uppercase btn-success btn-lg btn-block">Buscador de Disciplinas</a>
+                    <a href="{{ route('turma_trocas.novo') }}" role="button" class="btn text-uppercase btn-secondary btn-lg btn-block">Troca de Turma</a>
+                    <a href="{{ route('turma_trocas.index') }}" role="button" class="btn text-uppercase btn-secondary btn-lg btn-block">Visualizar minhas Trocas</a>
                 </div>
             </div>
         </div>
--- routes/web.php
+++ routes/web.php
@@ -18,3 +18,23 @@ Route::get('/', function () {
 Auth::routes();
 
 Route::get('/home', 'HomeController@index')->name('home');
+
+Route::get('/fila-de-atendimento', function(){
+    return "Fila de atendimento";
+})->name('filas.index');
+
+Route::get('/agendar-coordenador', function(){
+    return "Agendar coordenador";
+})->name('filas.agendar');
+
+Route::get('/buscador-de-disciplinas', function(){
+    return "Buscador de disciplinas";
+})->name('disciplinas.buscar');
+
+Route::get('/minhas-trocas', function(){
+    return "Minhas trocas";
+})->name('turma_trocas.index');
+
+Route::get('/troca-turmas/novo', function(){
+    return "Trocador";
+})->name('turma_trocas.novo');
